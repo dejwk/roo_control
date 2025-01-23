3d592097dffc4d5cb5d9d461f7a562f5753b04d3
dejwk <dawid.kurzyniec@gmail.com>
2025-01-23 19:34:51 +0100

    Added support for push buttons with basic gesture detection (click, double-click, long press).

---
+++ src/roo_control/push_button.cpp
@@ -0,0 +1,54 @@
+#include "roo_control/push_button.h"
+
+namespace roo_control {
+
+using roo_time::Interval;
+using roo_time::Millis;
+using roo_time::Uptime;
+
+static constexpr Interval kDebounceInterval = Millis(20);
+static constexpr Interval kLongPressInterval = Millis(500);
+static constexpr Interval kDoubleClickDeadline = Millis(500);
+
+PushButton::PushButton(BinarySelector& selector, BinaryLogicalState idle_state)
+    : selector_(selector),
+      idle_state_(idle_state),
+      state_(selector.getState()),
+      last_state_change_time_(Uptime::Now()),
+      prev_state_change_time_(last_state_change_time_),
+      last_click_time_(last_state_change_time_),
+      is_pressed_(state_ != idle_state) {}
+
+void PushButton::tick() {
+  Uptime now = Uptime::Now();
+  if (now - last_state_change_time_ < kDebounceInterval) return;
+  BinaryLogicalState new_state = selector_.getState();
+  if (new_state == state_) {
+    // Maybe long-press?
+    if (is_pressed_ && now - last_state_change_time_ > kLongPressInterval) {
+      onLongPress();
+      is_pressed_ = false;
+    }
+    return;
+  }
+  prev_state_change_time_ = last_state_change_time_;
+  last_state_change_time_ = now;
+  state_ = new_state;
+  if (state_ != idle_state_) {
+    is_pressed_ = true;
+    onDown();
+  } else {
+    onUp();
+    if (is_pressed_) {
+      if (now - last_click_time_ < kDoubleClickDeadline) {
+        onDoubleClick();
+      } else {
+        onClick();
+      }
+      last_click_time_ = now;
+      is_pressed_ = false;
+    }
+  }
+}
+
+}  // namespace roo_control
---
+++ src/roo_control/push_button.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "roo_control/selector.h"
+#include "roo_time.h"
+
+namespace roo_control {
+
+// Represents a human-pressable monentary button.
+//
+// This class implements debouncing and provides simple form of 'gesture'
+// detection.
+class PushButton : public BinarySelector {
+ public:
+  PushButton(BinarySelector& selector,
+             BinaryLogicalState idle_state = BINARY_STATE_HIGH);
+
+  BinaryLogicalState getState() const override { return state_; }
+
+  // Needs to be called periodically, and frequently (every 5-10 ms).
+  void tick();
+
+ protected:
+  // Overridable methods below are called from tick(). If you call tick() from a
+  // time-sensitive context, you may want to hand off the events to a queue in
+  // the implementation of these methods.
+
+  virtual void onDown() {}
+  virtual void onUp() {}
+  virtual void onClick() {}
+  virtual void onLongPress() {}
+  virtual void onDoubleClick() {}
+
+ private:
+  BinarySelector& selector_;
+  const BinaryLogicalState idle_state_;
+
+  BinaryLogicalState state_;
+  roo_time::Uptime last_state_change_time_;
+  roo_time::Uptime prev_state_change_time_;
+  roo_time::Uptime last_click_time_;
+  bool is_pressed_;
+};
+
+}  // namespace roo_control
