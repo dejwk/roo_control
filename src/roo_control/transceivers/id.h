a28848a9a484b7030b991a3a01c3114e84efe932
dejwk <dawid.kurzyniec@gmail.com>
2025-01-30 22:04:34 +0100

    Added sensor and actuator IDs.

--- src/roo_control/transceivers/id.h
+++ src/roo_control/transceivers/id.h
@@ -12,6 +12,8 @@ enum PredefinedTransceiverFamily {
 
 using TransceiverFamilyId = uint32_t;
 using TransceiverDeviceId = uint64_t;
+using SensorId = uint32_t;
+using ActuatorId = uint32_t;
 
 class UniversalTransceiverDeviceId {
  public:
@@ -30,7 +32,8 @@ class UniversalTransceiverDeviceId {
   TransceiverDeviceId uid_;
 };
 
-inline bool operator==(const UniversalTransceiverDeviceId& a, const UniversalTransceiverDeviceId& b) {
+inline bool operator==(const UniversalTransceiverDeviceId& a,
+                       const UniversalTransceiverDeviceId& b) {
   return a.family() == b.family() && a.uid() == b.uid();
 }
 
@@ -40,13 +43,44 @@ struct UniversalTransceiverDeviceIdHashFn {
   }
 };
 
+class UniversalSensorId {
+ public:
+  UniversalSensorId() : device_id_(), sensor_id_(0) {}
+
+  bool isDefined() const { return device_id_.isDefined(); }
+
+  UniversalSensorId(uint32_t family, uint64_t uid, uint32_t sensor_id)
+      : device_id_(family, uid), sensor_id_(sensor_id) {}
+
+  TransceiverFamilyId family() const { return device_id_.family(); }
+  TransceiverDeviceId device() const { return device_id_.uid(); }
+  SensorId sensor() const { return sensor_id_; }
+
+ private:
+  UniversalTransceiverDeviceId device_id_;
+  SensorId sensor_id_;
+};
+
+inline bool operator==(const UniversalSensorId& a, const UniversalSensorId& b) {
+  return a.family() == b.family() && a.device() == b.device() &&
+         a.sensor() == b.sensor();
+}
+
+struct UniversalSensorHashFn {
+  uint32_t operator()(const UniversalSensorId& val) const {
+    return (val.sensor() * 5 + val.device() * 61 + val.family() * 131);
+  }
+};
+
 }  // namespace roo_control
 
 namespace std {
 template <>
 struct hash<roo_control::UniversalTransceiverDeviceId> {
-  size_t operator()(const roo_control::UniversalTransceiverDeviceId& val) const {
+  size_t operator()(
+      const roo_control::UniversalTransceiverDeviceId& val) const {
     return ((val.uid() >> 32) * 5 + val.uid() * 11 + val.family() * 17);
   }
 };
+
 }  // namespace std
