679e9f6e07ab9f8b5c61d9fd725ad6eadafb4883
dejwk <dawid.kurzyniec@gmail.com>
2025-04-05 00:26:25 +0200

    Randomize the retry delay for inert switch.

--- src/roo_control/switch/inert_switch.cpp
+++ src/roo_control/switch/inert_switch.cpp
@@ -3,12 +3,14 @@
 namespace roo_control {
 
 roo_time::Interval DefaultBackoff(int retry_count) {
-  uint64_t min_delay_us = 200;      // 0.2 ms
-  uint64_t max_delay_us = 1000000;  // 1s
+  float min_delay_us = 1000.0f;     // 1 ms
+  float max_delay_us = 5000000.0f;  // 5 s
   float delay = pow(1.33, retry_count) * min_delay_us;
   if (delay > max_delay_us) {
-    return roo_time::Micros(max_delay_us);
+    delay = max_delay_us;
   }
+  // Randomize by +=20%, to make unrelated retries spread more evenly in time.
+  delay += (float)delay * ((float)rand() / RAND_MAX - 0.5f) * 0.4f;
   return roo_time::Micros((uint64_t)delay);
 }
 
